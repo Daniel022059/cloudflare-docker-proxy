410dddae91e1b6cafb532e7fa880e2799f52c94e
Daniel022059 <Daniel@njust.edu.cn>
2024-09-02 12:54:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.310388.xyz": dockerHub,
+  "quay.310388.xyz": "https://quay.io",
+  "gcr.310388.xyz": "https://gcr.io",
+  "k8s-gcr.310388.xyz": "https://k8s.gcr.io",
+  "k8s.310388.xyz": "https://registry.k8s.io",
+  "ghcr.310388.xyz": "https://ghcr.io",
+  "cloudsmith.310388.xyz": "https://docker.cloudsmith.io",
+  "ecr.310388.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.310388.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
